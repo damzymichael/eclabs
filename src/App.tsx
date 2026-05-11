0472eae7d17a82102ac2155f4d30993e6a1e760c
damzymichael <105460672+damzymichael@users.noreply.github.com>
2026-05-11 15:30:47 +0100

    scrolltotop

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Home from "./pages/Home"
 import BookAppointment from "./pages/BookAppointment"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import ScrollToTop from "./components/ScrollToTop"
 
 function App() {
   return (
     <Router>
       <div className="min-w-xs">
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
